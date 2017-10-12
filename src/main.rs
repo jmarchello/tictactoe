09f4f116425ae99fe551319ac98aa4cfeeb68e62
Josh Marchello <josh@jmarchello.com>
2017-10-11 22:14:13 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let board = [[" "; 3]; 3];
+    println!("\n");
+    println!(" {} | {} | {}", board[0][0], board[0][1], board[0][2]);
+    println!("------------");
+    println!(" {} | {} | {}", board[1][0], board[1][1], board[1][2]);
+    println!("------------");
+    println!(" {} | {} | {}", board[2][0], board[2][1], board[2][2]);
+    println!("\n");
+}
